ccb4e0c13839e93d2a1ffdaf687837a7af900746
okta62 <oktalestari2004@gmail.com>
2025-12-02 17:15:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,13 @@ date_default_timezone_set("Asia/Jakarta");
 
 echo "Hello Jenkins!<br>";
 echo "Pipeline PHP ini berjalan dengan sukses.<br>";
-echo "Waktu sekarang: " . date("Y-m-d H:i:s");
+echo "Waktu sekarang: " . date("Y-m-d H<?php
+date_default_timezone_set("Asia/Jakarta");
+
+echo "Hello Jenkins!<br>";
+echo "Pipeline PHP berjalan dengan sukses.<br>";
+echo "Waktu sekarang: " . date("Y-m-d H:i:s") . "<br>";
+echo "PHP version: " . phpversion() . "<br>";
+?>
+:i:s");
+
